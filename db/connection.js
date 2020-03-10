d04c1adc9535a44f8fa0844d3267903488ba4fe2
Brian Brown II <bkeithbii@gmail.com>
2020-03-10 08:51:05 -0400

    connnection.js changed

---
+++ db/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/bachelor_nation", {
+  useNewUrlParser: true
+});
+
+module.exports = mongoose;
--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const app = express();
 const parser = require("body-parser");
 const index = require("./routes/index");
 const cors = require("cors");
+
+app.use(cors());
+app.use(parser.json());
+app.use("/", index);
+
+app.set("port", process.env.PORT || 7000);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
