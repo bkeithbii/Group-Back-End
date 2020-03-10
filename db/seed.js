78ec4adf2518668bd2d42e4c2f3d217b837d36a4
Dennis Rivera <rivd19@outlook.com>
2020-03-10 10:11:08 -0400

    Built dataset for seeding

---
+++ db/seed.js
@@ -0,0 +1,16 @@
+const Movies = require("../models/Movies");
+const data = require("./data.json");
+
+const moviesData = data.map(params => {
+  const trendingMovies = {
+    id: params.id
+    // vote_average: params.vote_average,
+    // title: params.title,
+    // release_Date: params.release_Date,
+    // overview: params.overview,
+    // poster_path: params.poster_path
+  };
+
+  return trendingMovies;
+});
+console.log(moviesData);
