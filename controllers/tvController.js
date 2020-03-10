98a009b3f298c885bf63b44bf114e71ccafd8286
Brian Brown II <bkeithbii@gmail.com>
2020-03-10 14:18:53 -0400

    added routes to tv.js

--- controllers/tvController.js
+++ controllers/tvController.js
@@ -1,3 +1,24 @@
 const TV = require("../models/TV");
 
-module.exports = {};
+module.exports = {
+  index: (req, res) => {
+    TV.find({}).then(tvShow => {
+      res.json(tvShow);
+    });
+  },
+  showRating: (req, res) => {
+    TV.find({ rating: req.params.rating }).then(tvShow => {
+      res.json(tvShow);
+    });
+  },
+  showName: (req, res) => {
+    TV.find({ name: req.params.name }).then(tvShow => {
+      res.json(tvShow);
+    });
+  },
+  showReleaseDate: (req, res) => {
+    TV.find({ releaseDate: req.params.releaseDate }).then(tvShow => {
+      res.json(tvShow);
+    });
+  }
+};
