54625db2389fa4720e37dd35eeca33f9dc1a55a2
Brian Brown II <bkeithbii@gmail.com>
2020-03-10 16:28:43 -0400

    Added directory

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.use("/", index);
 
 // app.listen(3000, () => console.log("listening on port 3000"));
 
+app.get("/", function(req, res) {
+  res.json({
+    Directory: "Choose an option below",
+
+    Movies: "https://group-back-end.herokuapp.com/movie",
+    Shows: "https://group-back-end.herokuapp.com/tv"
+  });
+});
+
 app.set("port", process.env.PORT || 9090);
 app.listen(app.get("port"), () => {
   console.log(`✅ PORT: ${app.get("port")} 🌟`);
